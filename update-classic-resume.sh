45611ca0a6138f76f0621fdfc7a77c5ec1e2880d
Nibs <jpthek9@gmail.com>
2026-01-08 02:01:33 -0500

    Make update script pull

--- update-classic-resume.sh
+++ update-classic-resume.sh
@@ -1,1 +1,1 @@
-git subtree add --prefix=quills/classic_resume/packages/ttq-classic-resume https://github.com/nibsbin/ttq-classic-resume release/core --squash
+git subtree pull --prefix=quills/classic_resume/packages/ttq-classic-resume https://github.com/nibsbin/ttq-classic-resume release/core --squash
